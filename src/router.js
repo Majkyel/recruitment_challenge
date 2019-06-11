c8cb42fdc00f1b95c256333aa793049a747087fa
Michał Kuliś <michalkulis1993@gmail.com>
2019-06-12 00:32:20 +0200

    Add basic structure files to project.

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Status from './views/Status.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/status',
+      name: 'status',
+      component: Status,
+    },
+  ],
+});
