76dda4056227a946f3b45576f52229edf5a51719
Michał Kuliś <mkulis@eobuwie.com.pl>
2019-06-12 13:36:22 +0200

    Update content in App.vue component.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
-  <div id="app" class="app">
-    <Header />
-    <router-view />
-    <Footer />
-  </div>
+<div id="app" class="app">
+  <Header />
+  <router-view />
+  <Footer />
+</div>
 </template>
 
 <script>
-  import Header from './views/Header.vue';
-  import Footer from './views/Footer.vue';
+import Header from './views/Header.vue';
+import Footer from './views/Footer.vue';
 
-  export default {
-    components: {
-      Header,
-      Footer
-    }
+export default {
+  components: {
+    Header,
+    Footer
   }
+}
 </script>
 
 <style lang="scss">
 body {
-  margin: 0;
+    margin: 0;
 }
 .app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+.container {
+    max-width: 1366px;
+    margin: auto;
 }
 </style>
