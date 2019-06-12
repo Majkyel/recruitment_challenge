4a0093879f5836847b5a3817f05a63f44083a1b7
Michał Kuliś <mkulis@eobuwie.com.pl>
2019-06-12 11:34:22 +0200

    Add content in Footer.vue component.

---
+++ src/views/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="footer__container">
+        <div class="footer__content">
+            <p class="footer__text">All rights... or not.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    .footer__container {
+        .footer__content {
+            .footer__text {
+                padding: 10px 4px;
+                background-color: #000;
+                color: white;
+            }
+        }
+    }
+</style>
