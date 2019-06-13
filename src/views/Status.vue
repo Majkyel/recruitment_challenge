9fa499ba98b37c72e5bca4f31706c92782779311
Michał Kuliś <michalkulis1993@gmail.com>
2019-06-13 23:27:15 +0200

    Change content in views component.

--- src/views/Status.vue
+++ src/views/Status.vue
@@ -1,15 +1,18 @@
 <template>
 <div class="status">
+  <heroHeader />
   <machinesPower />
 </div>
 </template>
 
 <script>
+import heroHeader from '@/components/heroHeader.vue';
 import machinesPower from '@/components/status/machinesPowerInformation.vue';
 
 export default {
   components: {
-    machinesPower,
+    heroHeader,
+    machinesPower
   }
 }
 </script>
