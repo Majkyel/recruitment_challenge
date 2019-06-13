35270fa1bf763a894aea935cb88b89c43875595e
Michał Kuliś <mkulis@eobuwie.com.pl>
2019-06-13 11:55:59 +0200

    Add new content in homePageInformation.vue.

---
+++ src/components/home/homePageInformation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pageInformation">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'pageInformation',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
