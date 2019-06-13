f2fa7adaf4232c18a46d9dc77997db099024319c
Michał Kuliś <mkulis@eobuwie.com.pl>
2019-06-13 14:04:10 +0200

    Update machinesPowerinformation.vue content.

--- src/components/status/machinesPowerInformation.vue
+++ src/components/status/machinesPowerInformation.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="machinesPower">
-
-  </div>
+<div class="machinesPower">
+  <h1>This is an about page</h1>
+</div>
 </template>
 
 <script>
-
 export default {
   name: 'machinesPower',
 };
