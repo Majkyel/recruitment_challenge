c88055491f0a0a8d01bce3bc2dde683f9f1ee639
Michał Kuliś <mkulis@eobuwie.com.pl>
2019-06-13 11:56:13 +0200

    Add new content in machinesPowerInformation.vue.

---
+++ src/components/status/machinesPowerInformation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="machinesPower">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'machinesPower',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
