1d57385a2a23579bcd979ff29913b30ff4534364
Michał Kuliś <mkulis@eobuwie.com.pl>
2019-06-14 13:19:37 +0200

    Update style and add logic content in component.

--- src/components/status/machinesPowerInformation.vue
+++ src/components/status/machinesPowerInformation.vue
@@ -1,13 +1,19 @@
 <template>
 <div class="machinesPower">
   <div class="machinesPower__container container">
-    <h2 class="machinesPower__title">TOTAL POWER: xxx</h2>
+    <h2 class="machinesPower__title">TOTAL POWER: {{totalPower}} kW</h2>
     <div class="machinesPower__content">
-      <div class="machinesPower--consumption first-content">
-
+      <div class="machinesPower__content--consumption first-content">
+        <div class="machinesPower__image">
+          <img src="" alt="">
+        </div>
+        <div class="machinesPower--consumption">{{mileniumFalconPower}} kW</div>
       </div>
-      <div class="machinesPower--consumption second-content">
-
+      <div class="machinesPower__content--consumption second-content">
+        <div class="machinesPower__image">
+          <img src="" alt="">
+        </div>
+        <div class="machinesPower--consumption">{{lightsaberPower}} kW</div>
       </div>
     </div>
   </div>
@@ -17,9 +23,39 @@
 <script>
 export default {
   name: 'machinesPower',
+  data: function() {
+    return {
+      totalPower: 0,
+      mileniumFalconPower: 0,
+      lightsaberPower: 0,
+      mileniumIsActive: false,
+      lightsaberIsActive: false
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+.machinesPower {
+    .machinesPower__container {
+        padding: 100px 0;
+        .machinesPower__title {}
+        .machinesPower__content {
+            display: flex;
+            justify-content: space-around;
+            margin: 50px 0;
+            .machinesPower__content--consumption {
+                .machinesPower__image {
+                    height: 100px;
+                    width: 100px;
+                    border: 2px solid gray;
+                    border-radius: 50px;
 
+                }
+            }
+            .first-content {}
+            .second-content {}
+        }
+    }
+}
 </style>
