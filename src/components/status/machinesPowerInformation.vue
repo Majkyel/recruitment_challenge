a8705bee193d713d46758d641f949ac8e1153d9b
Michał Kuliś <mkulis@eobuwie.com.pl>
2019-06-19 14:43:36 +0200

    Update content component.

--- src/components/status/machinesPowerInformation.vue
+++ src/components/status/machinesPowerInformation.vue
@@ -5,13 +5,13 @@
     <div class="machinesPower__content">
       <div class="machinesPower__content--consumption first-content" @click="powerMechanismMileniumFalcon">
         <div class="machinesPower__image" :class={isActive:mileniumIsActive}>
-          <img src="" alt="Milenium Falcon">
+          <img class="image__first" src="../../../public/images/falcon.jpg" alt="Milenium Falcon">
         </div>
         <div class="machinesPower--consumption">{{mileniumFalconPower}} kW</div>
       </div>
       <div class="machinesPower__content--consumption second-content" @click="powerMechanismLightsaber">
         <div class="machinesPower__image" :class={isActive:lightsaberIsActive}>
-          <img src="" alt="Lightsaber">
+          <img class="image__second" src="../../../public/images/lightsaber.png" alt="Lightsaber">
         </div>
         <div class="machinesPower--consumption">{{lightsaberPower}} kW</div>
       </div>
@@ -65,6 +65,7 @@ export default {
 <style lang="scss" scoped>
 .machinesPower {
     .machinesPower__container {
+        margin: 20vh auto;
         .machinesPower__title {}
         .machinesPower__content {
             display: flex;
@@ -72,16 +73,37 @@ export default {
             margin: 50px 0;
             .machinesPower__content--consumption {
                 .machinesPower__image {
-                    height: 100px;
-                    width: 100px;
+                    height: 300px;
+                    width: 300px;
                     border: 2px solid gray;
-                    border-radius: 50px;
+                    box-shadow: 0 0 13px gray;
+                    border-radius: 100%;
+                    overflow: hidden;
+                    filter: saturate(8%);
                     img {
                         display: block;
+
+                        margin-left: auto;
+                        margin-right: auto;
+                        margin-top: 50%;
+                        transform: translateY(-50%);
                     }
+                    .image__first {
+                        width: 90%;
+                    }
+                    .image__second {
+                        width: 80%;
+                    }
+                }
+                .machinesPower--consumption {
+                    color: black;
+                    font-size: 24px;
+                    margin: 13px 0;
                 }
                 .isActive {
                     border: 2px solid green;
+                    box-shadow: 0 0 13px green;
+                    filter: saturate(100%);
                 }
             }
             .first-content {}
