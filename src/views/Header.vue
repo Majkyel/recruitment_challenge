2a7c933559a2ffbaa462e09c2a8cfa7ac34829b7
Michał Kuliś <michalkulis1993@gmail.com>
2019-06-22 01:10:08 +0200

    Add hamburger menu to mobile view in Header.vue.

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -7,13 +7,18 @@
           <Logo width="150px" height="100px" />
         </a>
       </div>
-      <div class="navigation__links">
-        <router-link class="navigation__link link" to="/">Home</router-link>
-        <router-link class="navigation__link link" to="/status">Status</router-link>
+      <div class="navigation__links" v-bind:class="{hamburger__navigation:hamburgerIsActive}">
+        <div class="navigation__container-links" v-bind:class="{hamburger__links:hamburgerIsActive}">
+          <router-link class="navigation__link link" to="/" @click.native="deactiveHamburgerMenu">Home</router-link>
+          <router-link class="navigation__link link" to="/status" @click.native="deactiveHamburgerMenu">Status</router-link>
+        </div>
       </div>
-      <div class="hamburger__menu">
-        <router-link class="hamburger__link link" to="/">Home</router-link>
-        <router-link class="hamburger__link link" to="/status">Status</router-link>
+      <div class="hamburger__menu" v-bind:class="{hamburger__animation:hamburgerIsActive}" @click="activeHamburgerMenu">
+        <div class="hamburger__container">
+          <div class="hamburger__bar1"></div>
+          <div class="hamburger__bar2"></div>
+          <div class="hamburger__bar3"></div>
+        </div>
       </div>
     </div>
   </div>
@@ -24,8 +29,31 @@
 import Logo from './Logo/Logo.vue';
 
 export default {
+  data: function() {
+    return {
+      hamburgerIsActive: false
+    }
+  },
   components: {
     Logo,
+  },
+  methods: {
+    activeHamburgerMenu: function() {
+      this.hamburgerIsActive = !this.hamburgerIsActive;
+      let body = document.querySelector('body');
+      if (this.hamburgerIsActive) {
+        body.style.overflow = "hidden";
+      } else {
+        body.style.overflow = "auto";
+      }
+    },
+    deactiveHamburgerMenu: function() {
+      let body = document.querySelector('body');
+      if (this.hamburgerIsActive) {
+        this.hamburgerIsActive = false;
+        body.style.overflow = "auto";
+      }
+    }
   }
 }
 </script>
@@ -64,10 +92,56 @@ export default {
                     font-weight: 900;
                 }
             }
+            .hamburger__navigation {
+                display: block!important;
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                height: calc(100vh - 101px);
+                background-color: white;
+                z-index: 2;
+                .hamburger__links {
+                    position: relative;
+                    top: 50%;
+                }
+            }
             .hamburger__menu {
                 display: none;
+                align-self: center;
+                cursor: pointer;
+                .hamburger__container {
+                    .hamburger__bar1,
+                    .hamburger__bar2,
+                    .hamburger__bar3 {
+                        width: 30px;
+                        height: 3px;
+                        background-color: #000;
+                        margin: 6px 0;
+                        transition: 0.4s;
+                    }
+                }
+            }
+            .hamburger__animation .hamburger__bar1 {
+                -webkit-transform: rotate(-45deg) translate(-5px, 6px);
+                transform: rotate(-45deg) translate(-5px, 6px);
+            }
+            .hamburger__animation .hamburger__bar2 {
+                opacity: 0;
+            }
+            .hamburger__animation .hamburger__bar3 {
+                -webkit-transform: rotate(45deg) translate(-6px, -8px);
+                transform: rotate(45deg) translate(-6px, -8px);
             }
         }
+    }
+}
+@media screen and (max-width: 420px) {
+    .navigation__links {
+        display: none!important;
+    }
+    .hamburger__menu {
+        display: flex!important;
     }
 }
 </style>
